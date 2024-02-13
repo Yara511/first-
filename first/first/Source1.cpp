18de3e036918a94afa63dc548ec56dc2727547e6
Yara511 <159902494+Yara511@users.noreply.github.com>
2024-02-13 13:56:06 +0200

    Add files via upload

---
+++ first/first/Source1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void main()
+
+{
+	int length;
+	int width;
+	int area;
+	 
+	cout<<"this programm to calculate the area of rectangle"<<endl;
+	//input
+	cout<<"please enter the length"<<endl;
+	cin>>length;
+	cout<<"please enter the width"<<endl;
+	cin>>width;
+	//processing
+	area=length*width;
+	//output
+	cout<<"area="<<area<<endl;
+	
+
+}
